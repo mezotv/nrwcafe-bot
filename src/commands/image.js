54e31c3b48efbb4dcf7cd168ea0ed111f98ad3bd
Dominik K <dominik@koch-bautechnik.de>
2022-11-26 21:00:52 +0100

    Added yuzu option

--- src/commands/image.js
+++ src/commands/image.js
@@ -1,4 +1,4 @@
-const {  SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 const Canvas = require('@napi-rs/canvas');
 const { request } = require('undici');
 
@@ -10,40 +10,57 @@ module.exports = {
       option.setName('category')
         .setDescription('The gif category')
         .setRequired(true)
-    .addChoices(
-      { name: 'Winter Mütze', value: 'santahat' },
-      { name: 'Schnee', value: 'snowoverlay' },
-      { name: 'Adventskranz', value: 'adventoverlay' },
-    )),
+        .addChoices(
+          { name: 'Winter Mütze', value: 'santahat' },
+          { name: 'Schnee', value: 'snowoverlay' },
+          { name: 'Adventskranz', value: 'adventoverlay' },
+          { name: 'Yuzu', value: 'yuzuoverlay' },
+        )),
 
   async execute(interaction, client) {
     interaction.deferReply({ ephemeral: false }).catch(err => console.error(err));
-    const { body } = await request(interaction.user.displayAvatarURL({ extension: 'png', size: 1024 }));
+    const { body } = await request(interaction.user.displayAvatarURL({
+      extension: 'png',
+      size: 1024
+    }));
 
     const canvas = Canvas.createCanvas(1024, 1024);
-		const context = canvas.getContext('2d');
+    const context = canvas.getContext('2d');
 
     const avatar = await Canvas.loadImage(await body.arrayBuffer());
 
     let link = ''
 
-    if (interaction.options.getString('category') === 'santahat') {
-      link = 'https://cdn.discordapp.com/attachments/981648706203955230/1046047801148190731/santahat.png'
-    } else if (interaction.options.getString('category') === 'snowoverlay') {
-      link = 'https://cdn.discordapp.com/attachments/981648706203955230/1046073867006902302/snowoverlay.png'
-    } else {
-      link = 'https://cdn.discordapp.com/attachments/981648706203955230/1046076551860584488/adventoverlay.png'
-    }
+    switch (interaction.options.getString('category')) {
+      case 'santahat':
+        link = 'https://cdn.discordapp.com/attachments/981648706203955230/1046047801148190731/santahat.png';    
+        context.drawImage(avatar, 0, 0, canvas.width, canvas.height);
+        break;
+      case 'snowoverlay':
+        link = 'https://cdn.discordapp.com/attachments/981648706203955230/1046073867006902302/snowoverlay.png';
+        context.drawImage(avatar, 0, 0, canvas.width, canvas.height);
+        break;
+      case 'adventoverlay':
+        link = 'https://cdn.discordapp.com/attachments/981648706203955230/1046076551860584488/adventoverlay.png';
+        context.drawImage(avatar, 0, 0, canvas.width, canvas.height);
+        break;
+      case 'yuzuoverlay':
+          link = 'https://cdn.discordapp.com/attachments/981648706203955230/1046150967336370236/yuzuoverlay.png';
+          context.drawImage(avatar, 263, 145, 495, 495);
+          
+      break;
+      }
 
     const hat = await Canvas.loadImage(link);
 
-
-
-
-    context.drawImage(avatar, 0, 0, canvas.width, canvas.height);
     context.drawImage(hat, 0, 0, canvas.width, canvas.height);
-    const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
+    const attachment = new AttachmentBuilder(await canvas.encode('png'), {
+      name: 'profile-image.png'
+    });
 
-   await interaction.editReply({ content: "Dein neues profil bild ist fertig!", files: [attachment] }).catch(err => console.error(err));
+    await interaction.editReply({
+      content: "Dein neues profil bild ist fertig!",
+      files: [attachment]
+    }).catch(err => console.error(err));
   },
 };
